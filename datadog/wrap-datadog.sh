e5b7407fa41fe610e3bd13bb2b54e66b43f4169e
Tim McCormack <tmccormack@edx.org>
2025-01-21 15:49:56 +0000

    Enable global service naming

--- datadog/wrap-datadog.sh
+++ datadog/wrap-datadog.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Run the provided arguments as a command, instrumented with Datadog
 
-# Used in prod config, so copied here to keep things similar
+# Used in edxapp prod config, so copied here to keep things similar
 export DD_DJANGO_USE_HANDLER_RESOURCE_FORMAT=true
 export DD_DJANGO_INSTRUMENT_MIDDLEWARE=false
-
+export DD_TRACE_REMOVE_INTEGRATION_SERVICE_NAMES_ENABLED=true
 
 ddtrace-run "$@"
