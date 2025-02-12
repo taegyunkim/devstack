40d8604ee74d7d2fc229f29d29b1859bf75a924c
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-02-12 19:55:29 +0000

    add agent and full-host-profiler

--- datadog/lms-server.sh
+++ datadog/lms-server.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 # Run the LMS as it would normally run in devstack.
 
-python /edx/app/edxapp/edx-platform/manage.py lms runserver 0.0.0.0:18000 --settings devstack
+# python /edx/app/edxapp/edx-platform/manage.py lms runserver 0.0.0.0:18000 --settings devstack
+
+SERVICE_VARIANT=lms
+SERVICE_PORT=18000
+
+export DJANGO_SETTINGS_MODULE=${SERVICE_VARIANT}.envs.devstack
+gunicorn \
+    -c /edx/app/edxapp/edx-platform/${SERVICE_VARIANT}/docker_${SERVICE_VARIANT}_gunicorn.py \
+    --name ${SERVICE_VARIANT} \
+    --bind=0.0.0.0:${SERVICE_PORT} \
+    --max-requests=1000 \
+    --access-logfile \
+    - ${SERVICE_VARIANT}.wsgi:application
--- datadog/wrap-datadog.sh
+++ datadog/wrap-datadog.sh
@@ -5,5 +5,9 @@
 export DD_DJANGO_USE_HANDLER_RESOURCE_FORMAT=true
 export DD_DJANGO_INSTRUMENT_MIDDLEWARE=false
 export DD_TRACE_REMOVE_INTEGRATION_SERVICE_NAMES_ENABLED=true
+export DD_PROFILING_ENABLED=true
+export DD_PROFILING_STACK_V2_ENABLED=true
+export DD_PROFILING_TIMELINE_ENABLED=true
 
+#ddprof --service lms-debug-native-v1 --preset cpu_live_heap --tags stack:v2-maxf --timeline --inlined_functions true ddtrace-run "$@"
 ddtrace-run "$@"
