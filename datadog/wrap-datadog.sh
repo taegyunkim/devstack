e4590b7095f6235b54fd34b2d2608ec93154f999
Taegyun Kim <taegyun.kim@datadoghq.com>
2025-05-27 17:53:15 +0000

    save configs

--- datadog/wrap-datadog.sh
+++ datadog/wrap-datadog.sh
@@ -8,12 +8,16 @@ export DD_TRACE_REMOVE_INTEGRATION_SERVICE_NAMES_ENABLED=true
 export DD_PROFILING_ENABLED=true
 export DD_PROFILING_STACK_V2_ENABLED=true
 export DD_PROFILING_TIMELINE_ENABLED=true
+export DD_PROFILING_TAGS=variant:py-spy
+export DD_PROFILING_MEMORY_ENABLED=false
+export DD_PROFILING_MAX_FRAMES=512
+# export _DD_PROFILING_STACK_V2_ADAPTIVE_SAMPLING_ENABLED=true
 
 # variant:vanilla
-ddprof --service lms-ddprof --tags=variant:vanilla --preset cpu_live_heap --timeline --inlined_functions true "$@"
+# ddprof --service lms-ddprof --tags=variant:vanilla --preset cpu_live_heap --timeline --inlined_functions true "$@"
 
-# variant:dd
-# DD_PROFILING_ENABLED=false ddprof --service lms-ddprof --tags=variant:dd --preset cpu_live_heap --timeline --inlined_functions true ddtrace-run "$@"
+# variant:dd, DD_PROFILING_ENABLED=false was set in the inner script lms-server.sh
+# ddprof --service lms-ddprof --tags=variant:dd --preset cpu_live_heap --timeline --inlined_functions true ddtrace-run "$@"
 
 # variant:v1
 # DD_PROFILING_STACK_V2_ENABLED=false ddprof --service lms-ddprof --tags=variant:v1 --preset cpu_live_heap --timeline --inlined_functions true ddtrace-run "$@"
@@ -21,8 +25,21 @@ ddprof --service lms-ddprof --tags=variant:vanilla --preset cpu_live_heap --time
 # variant:v2
 # ddprof --service lms-ddprof --tags=variant:v2 --preset cpu_live_heap --timeline --inlined_functions true ddtrace-run "$@"
 
-# variant:v2-patched
-# ddprof --service lms-ddprof --tags=variant:v2-patched --preset cpu_live_heap --timeline --inlined_functions true ddtrace-run "$@"
+# variant:v2-patch
+# ddprof --service lms-ddprof --tags=variant:v2-patch --preset cpu_live_heap --timeline --inlined_functions true ddtrace-run "$@"
+
+# variant:v2-2048
+# DD_PROFILING_MAX_FRAMES=2048 ddprof --service lms-ddprof --tags=variant:v2-2048 --preset cpu_live_heap --timeline --inlined_functions true ddtrace-run "$@"
 
 #ddtrace-run "$@"
 #"$@"
+
+# variant:sigtrap
+#ddprof --service lms-ddprof --tags=variant:sigtrap-clock --preset cpu_live_heap --timeline --inlined_functions true ddtrace-run "$@"
+
+
+# ddprof --service lms-ddprof --tags=variant:3.7.0-dev28-memoff --preset cpu_live_heap --timeline --inlined_functions true ddtrace-run "$@"
+#"$@"
+
+
+ddprof --service lms-ddprof --tags=variant:py-spy --preset cpu_live_heap --timeline --inlined_functions true py-spy top --subprocesses --nonblocking -- "$@"
